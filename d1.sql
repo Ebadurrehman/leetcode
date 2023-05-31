dcce62c940446f35ba54ce44840c34dfe6265e3e
Ebadurrehman <65074852+Ebadurrehman@users.noreply.github.com>
2023-05-31 10:11:39 +0500

    done

--- d1.sql
+++ d1.sql
@@ -1,1 +1,3 @@
 select name from customer where referee_id is null or referee_id<>2
+select p.firstName,p.lastName,a.city,a.state from person p left join address a on p.personId=a.personId
+select e2.name as Employee from employee e1 inner join employee e2 on e1.id=e2.managerId where e2.salary>e1.salary
