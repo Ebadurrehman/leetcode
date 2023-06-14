33ae8ac6f0be1b5e05ec689b75b9d8fcf43b1717
Ebadurrehman <65074852+Ebadurrehman@users.noreply.github.com>
2023-06-14 11:18:00 +0500

    day 8 qs3

--- d8.sql
+++ d8.sql
@@ -7,3 +7,5 @@ Select actor_id,director_id
 From ActorDirector
 GROUP BY actor_id,director_id
 having count(timestamp)>=3
+# Write your MySQL query statement below
+select p.product_name,s.year,s.price from sales s left join product p on s.product_id=p.product_id
