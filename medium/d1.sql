1f90c8b96b95b32c4db4e385c17e7feb9c0761ad
Ebadurrehman <65074852+Ebadurrehman@users.noreply.github.com>
2023-07-06 11:40:05 +0500

    qs3

--- medium/d1.sql
+++ medium/d1.sql
@@ -6,3 +6,11 @@ BEGIN
     )t1 where rn=N
   );
 END
+
+# Write your MySQL query statement below
+select score,dense_rank() over(order by score desc) as 'rank' from scores
+
+# Write your MySQL query statement
+select distinct num as ConsecutiveNums from (
+select num, lag(num,1) over() as  prev,lead(num,1) over() as after,lead(num,2)over() as cc  from logs
+)t where num=prev and num=after 
